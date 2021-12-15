3b7006b741c4031017f9ae5231c5f07de1705fe0
gaito yamada <gaito021229@au.com>
2021-12-15 10:23:10 +0900

    static

---
+++ Ex29/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex29
+{
+    class Box
+    {
+
+        public static float GetVolume(float width, float height, float depth)
+        {
+            return width * height * depth;
+        }
+        public static float GetSurface(float width,float height,float depth)
+        {
+            return (width * height + height * depth + depth * width) * 2;
+        }
+    }
+}
---
+++ Ex29/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"box3の表面積={Box.GetSurface(2, 3.5f, 2)}");
+            Console.WriteLine($"box3の体積={Box.GetVolume(1, 3.5f, 2)}");           
+        }
+    }
+}
